eb48650b35fdb1dc85e82aaf8edb043545d4bc21
Priyam Garg <gargpriyam21@gmail.com>
2022-02-09 21:38:45 -0600

    Add user inputs for client to request certain files and how many times to request

--- CoAP/CoAP_Client_HW2.py
+++ CoAP/CoAP_Client_HW2.py
@@ -1,38 +1,53 @@
 #!/usr/bin/env python3
 
-# This file is part of the Python aiocoap library project.
+# This file is adapted from the Python aiocoap library project.
 #
 # Copyright (c) 2012-2014 Maciej Wasilak <http://sixpinetrees.blogspot.com/>,
 #               2013-2014 Christian Amsüss <c.amsuess@energyharvesting.at>
 #
+# Editted by Jordan Boerger (2022)
+#
 # aiocoap is free software, this file is published under the MIT license as
 # described in the accompanying LICENSE file.
 
-"""This is a usage example of aiocoap that demonstrates how to implement a
-simple client. See the "Usage Examples" section in the aiocoap documentation
-for some more information."""
-
-import logging
 import asyncio
 from sys import getsizeof
 
 from aiocoap import *
 
-logging.basicConfig(level=logging.INFO)
-
 async def main():
     protocol = await Context.create_client_context()
 
-    request = Message(code=GET, uri='coap://localhost/1MB')
+    print("What file would you like to request: 100B, 10KB, 1MB, 10MB")
+    requestedFile = input()
 
-    try:
-        response = await protocol.request(request).response
-    except Exception as e:
-        print('Failed to fetch resource:')
-        print(e)
+    if requestedFile == "100B" or requestedFile == "10KB" or requestedFile == "1MB" or requestedFile == "10MB":
+        print("Getting file: " + requestedFile)
     else:
-        print(f"Payload: {response.payload}")
-        print(f"Size: {getsizeof(response.payload)}")
+        print("Invalid File")
+        exit()
+
+    print("How many times do you want to receive the file?")
+    requestedRepeats = input()
+
+    if requestedRepeats.isdigit() and int(requestedRepeats) != 0:
+        print("Getting " + requestedFile + " " + requestedRepeats + " times")
+    else:
+        print("Error: Invalid input. Must be positive integer")
+        exit()
+
+    for i in range(1, int(requestedRepeats)+1):
+        request = Message(code=GET, uri='coap://localhost/' + requestedFile)
+
+        try:
+            response = await protocol.request(request).response
+        except Exception as e:
+            print('Failed to fetch resource:')
+            print(e)
+        else:
+            #print(f"Payload: {response.payload}")
+            print(f"Payload Number: {i}")
+            print(f"Size: {getsizeof(response.payload)}")
 
 if __name__ == "__main__":
     asyncio.run(main())
